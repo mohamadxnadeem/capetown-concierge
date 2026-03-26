e1f3a31e9c5ef5cd201c9772eeacecf5d5f53a8f
Lord of the Metaverse <107515774+mohamadxnadeem@users.noreply.github.com>
2026-03-26 18:04:58 +0000

    style fix

---
+++ styles/styled.d.ts
@@ -0,0 +1,31 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      primaryDark: string;
+      white: string;
+      heading: string;
+      textMuted: string;
+      background: string;
+      backgroundSoft: string;
+      border: string;
+    };
+
+    breakpoints: {
+      sm: string;
+      md: string;
+      lg: string;
+    };
+
+    radius: {
+      lg: string;
+    };
+
+    shadows: {
+      soft: string;
+      card: string;
+    };
+  }
+}
